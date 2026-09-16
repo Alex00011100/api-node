ad58e9d721c581eba9af0e267e544976cb20c762
Alex Galarza García <alecachogalarza@live.com.ar>
2026-09-15 22:25:18 -0300

    Cambiada tabla de estados

--- init.sql
+++ init.sql
@@ -3,31 +3,34 @@ CREATE DATABASE IF NOT EXISTS Api_tercero;
 
 USE Api_tercero;
 
-CREATE TABLE IF NOT EXISTS estados_usuario (
+-- Tabla: estados_usuario
+CREATE TABLE public.estados_usuario (
     id SERIAL PRIMARY KEY,
-    descripcion VARCHAR(50) NOT NULL UNIQUE
+    nombre VARCHAR(50) NOT NULL UNIQUE,
+    descripcion VARCHAR(255)
 );
 
-INSERT INTO estados_usuario (descripcion) 
-VALUES ('Activo'), ('Inactivo')
-ON CONFLICT (descripcion) DO NOTHING;
+INSERT INTO public.estados_usuario (id, nombre, descripcion) VALUES
+(1, 'estándar', 'usuario estándar'),
+(2, 'Moderador', 'Posee algunos permisos'),
+(3, 'Admin', 'Administrador, permiso total');
 
 -- Tabla: usuarios
-CREATE TABLE usuarios (
+CREATE TABLE public.usuarios (
     id SERIAL PRIMARY KEY,
     nombre VARCHAR(100) NOT NULL,
     usuario VARCHAR(100) NOT NULL,
     email VARCHAR(255) NOT NULL UNIQUE,
     password_hash VARCHAR(255) NOT NULL,
-    id_rol INTEGER REFERENCES estados_usuario(id),
+    id_rol INTEGER REFERENCES public.estados_usuario(id),
     activo BOOLEAN DEFAULT TRUE,
     fecha_creacion TIMESTAMP WITHOUT TIME ZONE DEFAULT CURRENT_TIMESTAMP
 );
 
 -- Tabla: publicaciones
-CREATE TABLE publicaciones (
+CREATE TABLE public.publicaciones (
     id SERIAL PRIMARY KEY,
     titulo VARCHAR(150) NOT NULL,
     contenido TEXT,
-    autor_id INTEGER REFERENCES usuarios(id) ON DELETE CASCADE
+    autor_id INTEGER REFERENCES public.usuarios(id) ON DELETE CASCADE
 );
